d7f5f6e8442029776fa796ffc5250a00c623802d
EvanildoLeal <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l>
2025-08-29 18:59:45 -0300

    add previsão de vendas

---
+++ live03/previsao_vendas/target/debug/deps/previsao_vendas.d
@@ -0,0 +1,5 @@
+C:\Users\evnld\live_rust_2025\live03\previsao_vendas\target\debug\deps\previsao_vendas.d: src\main.rs
+
+C:\Users\evnld\live_rust_2025\live03\previsao_vendas\target\debug\deps\previsao_vendas.exe: src\main.rs
+
+src\main.rs:
---
+++ live03/previsao_vendas/target/debug/previsao_vendas.d
@@ -0,0 +1,1 @@
+C:\Users\evnld\live_rust_2025\live03\previsao_vendas\target\debug\previsao_vendas.exe: C:\Users\evnld\live_rust_2025\live03\previsao_vendas\src\main.rs
